1f2af8628f9fcdd0bf0243f3213b2ca6aafb4d76
jimmy <jimmyhd_19@hotmail.com>
2022-08-22 20:03:32 -0500

    cambios

--- src/app/star-rating/star-rating.component.ts
+++ src/app/star-rating/star-rating.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
 	selector: 'app-star-rating',
 	templateUrl: './star-rating.component.html',
-	styleUrls: ['./star-rating.component.scss']
+	styleUrls: ['./star-rating.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: StarRatingComponent,
+			multi: true
+		}
+	]
 })
 export class StarRatingComponent implements OnInit {
 	private _rating: number = 3;
-	private _starCount: number = 5;
-
 	ratingArr: number[] = [];
 
 	ngOnInit(): void {
-		for (let index = 0; index < this._starCount; index++) {
+		for (let index = 0; index < 5; index++) {
 			this.ratingArr.push(index);
 		}
 	}
+
 	onClick(rating: number): void {
 		console.log(rating);
 		this._rating = rating;
